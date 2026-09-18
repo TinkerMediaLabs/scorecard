11f056e4a5b951d16d4286b5736a059149fb162d
TinkerMedia <admin@tinkermedia.net>
2026-09-18 09:10:20 -0500

    generals

--- app.config.js
+++ app.config.js
@@ -16,7 +16,7 @@ module.exports = {
   expo: {
     name: APP_NAME,
     slug: 'scorecard',
-    version: '1.1.0',
+    version: '1.1.1',
     orientation: 'default',
     icon: './assets/images/icon.png',
     scheme: 'scorecard',
